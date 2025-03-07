d2ceb65f99d1405e109029c72ef16ccc9560607e
Yaroslav Smolyaninov <yarichok.2019@bk.ru>
2025-03-07 19:23:48 +0700

    fix: add CORS on server

--- src/rest_app.py
+++ src/rest_app.py
@@ -1,10 +1,19 @@
-
+import os
+from fastapi.middleware.cors import CORSMiddleware
 from src.app.custom_app import CustomApp
 from src.routers.menu_router import menu_router
 
 
 app = CustomApp(
     title="MenuService"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[os.getenv('FRONTEND_URL')],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(
